10fbe303578cce9b1c486e37d0676c60a381f24e
caro368e <caro368e@stud.kea.com>
2020-02-11 10:25:26 +0100

    pop-up indsat

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ let filter = "alle";
 
 const container = document.querySelector("#data-container");
 const oversigtTemplate = document.querySelector("template");
+const detalje = document.querySelector("#detalje");
 
 document.addEventListener("DOMContentLoaded", start);
 
@@ -21,7 +22,7 @@ async function hentData() {
     visRetter();
 }
 
-visRetter();
+
 
 function visRetter() {
     container.innerHTML = "";
@@ -36,11 +37,22 @@ function visRetter() {
             klon.querySelector("img").src = `imgs/small/${
                 ret.gsx$billede.$t}-sm.jpg`;
 
+            klon.querySelector("article").addEventListener("click", function () {
+                visDetalje(detalje);
+            });
+
             container.appendChild(klon);
         }
 
-    })
+    });
 }
+
+function visDetalje(person) {
+    detalje.classList.remove("skjul");
+    detalje.querySelector("button").addEventListener("click", () => detalje.classList.add("skjul"));
+    console.log(person.gsx$navn);
+}
+
 
 function addEventListenersToButtons() {
     document.querySelectorAll(".filter").forEach(elm => {
